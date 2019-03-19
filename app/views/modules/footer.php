12ae01fea98d71d93190cf53400e60a55d2724b3
Ignacio Morinigo <morinacho@gmail.com>
2019-03-19 01:15:02 -0300

    prueba de promise, conexiones asincronica con ajax

--- app/views/modules/footer.php
+++ app/views/modules/footer.php
@@ -3,6 +3,7 @@
 		<script type="text/javascript" src="<?php echo URL_ROUTE;?>js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="<?php echo URL_ROUTE;?>js/bootstrap.bundle.min.js"></script>
 		<script type="text/javascript" src="<?php echo URL_ROUTE;?>js/isotope.pkgd.min.js"></script>
+		<script type="text/javascript" src="<?php echo URL_ROUTE;?>js/promise.js"></script>
 		<script type="text/javascript" src="<?php echo URL_ROUTE;?>js/script.js"></script>
 	</body>
 </html>
