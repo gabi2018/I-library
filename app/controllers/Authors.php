968210c28325240fabddd13dac8920181e1130fe
Alvaroochoa85 <alvaroemmanuelochoa85@gmail.com>
2019-03-21 19:58:45 -0300

    cree el modelo, controler y vista de author

---
+++ app/controllers/Authors.php
@@ -0,0 +1,29 @@
+<?php
+	class Authors extends Controller{
+		private $authorModel;
+
+		public function __construct(){
+			//se crea el objeto y se lo asigna al modelo
+			$this->authorModel = $this->model('Author');
+			session_start();
+		}
+		public function create(){
+			//se le pasa por parametro el autor a la vista
+			$this->view('authors/create');
+		}
+
+		public function store(){
+			//el metodo request pregunta qe metdo utilizar, si GET O POST
+			if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['register'])){
+				if(!empty($_POST['author-name']) && !
+					empty($_POST['author-lastname'])){
+					$param = [
+						'author-name' => trim()]
+				}
+			}
+		}
+	}
+
+
+
+?>
