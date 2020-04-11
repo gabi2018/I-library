fe6f21b26d7290094bc8fcdb669d34cca5671315
Ignacio Morinigo <morinacho@gmail.com>
2020-04-11 03:07:40 -0300

    Creacion de login y menu lateral

--- app/assets/Controller.php
+++ app/assets/Controller.php
@@ -9,42 +9,22 @@
 
 		# Load view
 		public function view($view, $param = []){
-			if (file_exists('../app/views/' . $view . '.php')){
-				require_once '../app/views/' . $view . '.php';
+			session_start();
+			if(Controller::authenticated()){
+				if (file_exists('../app/views/' . $view . '.php')){
+					require_once '../app/views/' . $view . '.php';
+				}
+				else{
+					require_once '../app/views/modules/404/index.html';
+				}
 			}
 			else{
-				#require_once '../app/views/modules/404/index.html';
-				die('ERROR');
+				require_once '../app/views/modules/login.php';
 			}
 		}
 
 		public static function authenticated(){
-			session_start();
-			if (!isset($_SESSION['user'])){
-			    return false;
-			}
-			else{
-				return true;
-			}
-		}
-
-		# Delete possible injections 
-		public static function deleteSpecialChars($param, $type){		
-			switch ($type) {
-				case 'email':
-					$filter = FILTER_SANITIZE_EMAIL;
-					break;
-				case 'int':
-					$filter = FILTER_SANITIZE_NUMBER_INT;
-					break;
-				case 'float':
-					$filter = FILTER_SANITIZE_NUMBER_FLOAT;
-					break;
-				case 'char':
-					$filter = FILTER_SANITIZE_SPECIAL_CHARS;
-					break;
-			}
-			return filter_var(htmlspecialchars(trim($param)), $filter);
-		}
+			return (isset($_SESSION['user']));
+		} 
 	}
 ?>
