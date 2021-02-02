b2ae5b31b83067d3770172244ea0fb1e658798d7
root <morinacho@gmail.com>
2021-02-01 22:56:15 -0300

    ui loan

--- app/books/controller/Books.php
+++ app/books/controller/Books.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 	class Books extends Controller{
 		private $booksModel;
 		private $authorModel;
@@ -13,54 +13,55 @@
 			$this->booksModel      = $this->model('Book');
 			$this->authorModel     = $this->model('Author', 'authors');
 			$this->authorTypeModel = $this->model('AuthorType', 'authortypes');
-			$this->editorialModel  = $this->model('Editorial', 'editorials');	 	
+			$this->editorialModel  = $this->model('Editorial', 'editorials');
 			$this->languajeModel   = $this->model('Languaje', 'languajes');
-			$this->topicModel      = $this->model('Topic', 'topics');	
-			$this->categoryModel   = $this->model('Category','categories');
-			$this->authorHasBookModel = $this->model('AuthorHasBook','authorshasbooks'); 
+			$this->topicModel      = $this->model('Topic', 'topics');
+			$this->categoryModel   = $this->model('Category', 'categories');
+			$this->authorHasBookModel = $this->model('AuthorHasBook', 'authorshasbooks');
 		}
-	
-		public function show(){ 
+
+		public function show(){
 			$this->view('show');
-		} 
+		}
 
 		public function index(){
 			$this->view('index');
-		}  
+		}
 
-		public function create(){  
+		public function create(){
 			$param = [
-				"languajes"   => $this->languajeModel->getLanguajes(), 
+				"languajes"   => $this->languajeModel->getLanguajes(),
 				"authors"	  => $this->authorModel->getAuthors(),
 				"authortypes" => $this->authorTypeModel->getAuthorTypes(),
 				"editorials"  => $this->editorialModel->getEditorials(),
 				"topics"	  => $this->topicModel->getTopics()
- 			];
-			$this->view('create', $param); 
+			];
+			$this->view('create', $param);
 		}
 
-		public function store(){ 
-			if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['book-register'])){
-				if(isset($_POST['book-title']) && isset($_POST['book-isbn']) && 
-				   isset($_POST['book-languaje']) && isset($_POST['category-topic']) && 
-				   isset($_POST['book-single']) && isset($_POST['editorial-id']) && 
-				   isset($_POST['book-topo']) && isset($_POST['book-cata'])){
-						   
-					 $book_status = 1;//disponible
-					 $cantidadBook=$_POST['book-cant'];
+		public function store(){
+			if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['book-register'])){
+				if (
+					isset($_POST['book-title']) && isset($_POST['book-isbn']) &&
+					isset($_POST['book-languaje']) && isset($_POST['category-topic']) &&
+					isset($_POST['book-single']) && isset($_POST['editorial-id']) &&
+					isset($_POST['book-topo']) && isset($_POST['book-cata'])
+				){
+
+					$book_status = 1; //disponible
+					$cantidadBook = $_POST['book-cant'];
 					if ($_POST['book-single'] == 1){
-						$book_status = 2;//copia unica
-						$cantidadBook=1;
+						$book_status = 2; //copia unica
+						$cantidadBook = 1;
 					}
-					
-				  
+
 					$param = [
 						'book-title'	 => trim($_POST['book-title']),
 						'book-isbn'		 => trim($_POST['book-isbn']),
 						'book-img'		 => $_FILES['book-img'],
 						'book-pages'	 => trim($_POST['book-pages']),
 						'book-category'  => trim($_POST['category-topic']),
-		
+
 						'book-desc'		 => trim($_POST['book-desc']),
 						'book-editorial' => trim($_POST['editorial-id']),
 						'book-vol'		 => trim($_POST['book-vol']),
@@ -68,239 +69,230 @@
 						'book-year'		 => trim($_POST['book-year']),
 						'book-topo'		 => trim($_POST['book-topo']),
 						'book-languaje'  => trim($_POST['book-languaje']),
-					
+
 						'book-authors'	 => $_POST['author-list'],
 						'book-status_id' => $book_status,
 						'book-cata'		 => trim($_POST['book-cata']),
 					];
-															
 
-					if($this->booksModel->addBook($param,1,$cantidadBook)){
-						echo '<p>guardado con exito<p>';	
-						redirect('books/index');		
-					}	
-					else{
-						echo"FATAL ERROR";
+					if ($this->booksModel->addBook($param, 1, $cantidadBook)) {
+						echo '<p>guardado con exito<p>';
+						redirect('books/index');
+					} 
+					else {
+						echo "FATAL ERROR";
 					}
 				}
-				echo"error no entro "; 
-			}		 
+				echo "error no entro ";
+			}
 		}
 
 		public function search(){
-			if(isset($_POST['search'])){
+			if (isset($_POST['search'])) {
 				$param = ['book' => trim($_POST['search'])];
 				$books = $this->booksModel->getBooksTitle($param);
 
 				foreach ($books as $book) {
 					echo "<div class='card book-list col-2 mr-3 mb-3'>
-							<a href='" .URL_ROUTE ."books/read/$book->book_topolographic '><img class='card-img-top' src=' " .URL_ROUTE."media/images/book/$book->book_img'style='width:100%'>
-							<p>$book->book_title </p></a>";
-					
+								<a href='" . URL_ROUTE . "books/read/$book->book_topolographic '><img class='card-img-top' src=' " . URL_ROUTE . "media/images/book/$book->book_img'style='width:100%'>
+								<p>$book->book_title </p></a>";
+
 					$authorHasBook = $this->authorHasBookModel->getAutorTipe($book->book_id);
-					foreach ($authorHasBook as $authorsHasBook) { 
-						echo"<p>$authorsHasBook->author_name $authorsHasBook->author_lastname</p>";
+					foreach ($authorHasBook as $authorsHasBook) {
+						echo "<p>$authorsHasBook->author_name $authorsHasBook->author_lastname</p>";
 					}
 					echo "<input type='hidden' name='isbn' value='$book->book_isbn'></div>";
 				}
 			}
 		}
-		
+
 
 		public function searchForLoan(){
-			if(isset($_POST['search'])){
+			if (isset($_POST['search'])) {
 				$param = ['book' => trim($_POST['search'])];
 				$books = $this->booksModel->getBooksTitle($param);
 
 				foreach ($books as $book) {
-					echo "<li class='option'><div class='card book-list col-2 mr-3 mb-3'>
-							 '<img class='card-img-top' src=' " .URL_ROUTE."media/images/book/$book->book_img'style='width:100%'>
-								</div>							
-								<div><p style='color:#343a40;font-size:100%;font-family:arial black'>$book->book_title</p>";
-					
+					echo "
+						<li class='option'>
+							<div class='col-12 p-1'>
+								<div class='media'>
+									<img class='mr-3' src='" . URL_ROUTE . "media/images/book/$book->book_img' alt='User profile img' style='width: 50px; height:70px;'>
+									<div class='media-body'>
+										<h5 class='mt-0'>$book->book_title</h5>";
 					$authorHasBook = $this->authorHasBookModel->getAutorTipe($book->book_id);
-					foreach ($authorHasBook as $authorsHasBook) { 
-						echo"<p style='font-size:80%'>$authorsHasBook->author_name $authorsHasBook->author_lastname</p>";
+					foreach ($authorHasBook as $authorsHasBook) {
+						echo "<p style='font-size:80%'>$authorsHasBook->author_name $authorsHasBook->author_lastname</p>";
 					}
-					echo "<input type='hidden' name='isbn' value='$book->book_isbn'></div></li>";
+					echo "<input type='hidden' name='isbn' value='$book->book_isbn'></div><span class='material-icons'>event_note</span></li>"; 					
 				}
 			}
 		}
-		public function read($topolographic){
+		
+		public function read($topolographic)
+		{
 			//consultar book
 			$books = $this->booksModel->getBook($topolographic);
-			$author=$this->authorModel->getAhutoresBook($topolographic);
-			$categoryTemaSubtema=$this->categoryModel->getCategoSubteTema($topolographic);
-			
-			$param=[
-				'book'=> $books,
-				'author'=> $author,
-				'category'=>$categoryTemaSubtema,
-				'cantidad-ejemplares-disponi'=> $this->booksModel->countBookAvailability($books->book_isbn,'Disponible'),
-				'cantidad-ejemplares-noDispo'=>$this->booksModel->countBookAvailability($books->book_isbn,'No disponible'),
-				'cantidad-ejemplares-prestados'=>$this->booksModel->countBookAvailability($books->book_isbn,'Prestado'),
-				'cantidad-ejemplares-reservados'=>$this->booksModel->countBookAvailability($books->book_isbn,'Reservado'),
-				'cantidad-ejemplares-copiaUnica'=>$this->booksModel->countBookAvailability($books->book_isbn,'Copia unica'),							
+			$author = $this->authorModel->getAhutoresBook($topolographic);
+			$categoryTemaSubtema = $this->categoryModel->getCategoSubteTema($topolographic);
+
+			$param = [
+				'book' => $books,
+				'author' => $author,
+				'category' => $categoryTemaSubtema,
+				'cantidad-ejemplares-disponi' => $this->booksModel->countBookAvailability($books->book_isbn, 'Disponible'),
+				'cantidad-ejemplares-noDispo' => $this->booksModel->countBookAvailability($books->book_isbn, 'No disponible'),
+				'cantidad-ejemplares-prestados' => $this->booksModel->countBookAvailability($books->book_isbn, 'Prestado'),
+				'cantidad-ejemplares-reservados' => $this->booksModel->countBookAvailability($books->book_isbn, 'Reservado'),
+				'cantidad-ejemplares-copiaUnica' => $this->booksModel->countBookAvailability($books->book_isbn, 'Copia unica'),
 			];
 
-			$this->view('book',$param);
+			$this->view('book', $param);
 		}
-		public function edit($topolographic){
+		public function edit($topolographic)
+		{
 
 			$books = $this->booksModel->getBook($topolographic);
-			$author=$this->authorModel->getAhutoresBook($topolographic);
-			$categoryTemaSubtema=$this->categoryModel->getCategoSubteTema($topolographic);
-			
-			$param=[
-				'book'=> $books,
-				'author'=> $author,
-				'category'=>$categoryTemaSubtema,
-				'languajes'  => $this->languajeModel->getLanguajes(), 
+			$author = $this->authorModel->getAhutoresBook($topolographic);
+			$categoryTemaSubtema = $this->categoryModel->getCategoSubteTema($topolographic);
+
+			$param = [
+				'book' => $books,
+				'author' => $author,
+				'category' => $categoryTemaSubtema,
+				'languajes'  => $this->languajeModel->getLanguajes(),
 				'authors'	  => $this->authorModel->getAuthors(),
 				'authortypes' => $this->authorTypeModel->getAuthorTypes(),
 				'editorials'  => $this->editorialModel->getEditorials(),
 				'topics'	  => $this->topicModel->getTopics(),
-				'cantidad-ejemplares'=> $this->booksModel->countBook($books->book_isbn),
+				'cantidad-ejemplares' => $this->booksModel->countBook($books->book_isbn),
 			];
 
 			$this->view('edit', $param);
-			
-
 		}
 
-		public function update(){
-			if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['book-update'])){
-				if(isset($_POST['book-title']) && isset($_POST['book-isbn'])
+		public function update()
+		{
+			if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['book-update'])) {
+				if (
+					isset($_POST['book-title']) && isset($_POST['book-isbn'])
 					&& isset($_POST['book-languaje']) && isset($_POST['category-topic'])
-					&&isset($_POST['editorial-id'])&& isset($_POST['book-topo']) && isset($_POST['book-cata'])){
-					
-									$isbnViejo=$_POST['book-isbn-viejo'];
-									$Ids=$this->booksModel->getIdsBooks($isbnViejo);
-									$book_status = 1;//disponible
-									$cantidadBook=$_POST['book-cantidad'];
-									$cantidadVieja=$_POST['book-cantidad-vieja'];
-									if ($cantidadBook == 1){
-									$book_status = 2;//copia unica
-									$cantidadBook=1;
-								}
-							
-							$i=1;
-								foreach ($Ids as $id) {																							
-												$param = [
-													'book-id' 	  => $id->book_id, 
-													'book-title'	 => trim($_POST['book-title']),
-													'book-isbn'		 => trim($_POST['book-isbn']),
-													'book-img'		 => $_FILES['book-img'],
-													'ext-img-vieja'=> trim($_POST['ext-vieja']),
-													'book-pages'	 => trim($_POST['book-pages']),
-													'book-category'  => trim($_POST['category-topic']),
-												
-													'book-desc'		 => trim($_POST['book-desc']),
-													'book-editorial' => trim($_POST['editorial-id']),
-													'book-vol'		 => trim($_POST['book-vol']),
-													'book-edition'	 => trim($_POST['book-edition']),
-													'book-year'		 => trim($_POST['book-year']),
-													'book-topo'		 => trim($_POST['book-topo']).'-'.$i,
-													'book-languaje'  => trim($_POST['book-languaje']),
-											
-													'book-authors'	 => $_POST['author-list'],
-													'book-status_id' => $book_status,
-													'book-cata'		 => trim($_POST['book-cata']),
-												];
+					&& isset($_POST['editorial-id']) && isset($_POST['book-topo']) && isset($_POST['book-cata'])
+				) {
 
-												$this->booksModel->editBook($param,$i);
-												$i++;
-								}
-																	 							
-							if($cantidadBook>$cantidadVieja){
-								$param = [
-													'book-title'	 => trim($_POST['book-title']),
-													'book-isbn'		 => trim($_POST['book-isbn']),
-													'book-img'		 => $_FILES['book-img'],
-													'ext-img-vieja'=> trim($_POST['ext-vieja']),
-													'book-pages'	 => trim($_POST['book-pages']),
-													'book-category'  => trim($_POST['category-topic']),											
-													'book-desc'		 => trim($_POST['book-desc']),
-													'book-editorial' => trim($_POST['editorial-id']),
-													'book-vol'		 => trim($_POST['book-vol']),
-													'book-edition'	 => trim($_POST['book-edition']),
-													'book-year'		 => trim($_POST['book-year']),
-													'book-topo'		 => trim($_POST['book-topo']),
-													'book-languaje'  => trim($_POST['book-languaje']),											
-													'book-authors'	 => $_POST['author-list'],
-													'book-status_id' => $book_status,
-													'book-cata'		 => trim($_POST['book-cata']),
-												];
-								
-								$this->booksModel->addBook($param,$cantidadVieja+1,$cantidadBook);																																
-							}
+					$isbnViejo = $_POST['book-isbn-viejo'];
+					$Ids = $this->booksModel->getIdsBooks($isbnViejo);
+					$book_status = 1; //disponible
+					$cantidadBook = $_POST['book-cantidad'];
+					$cantidadVieja = $_POST['book-cantidad-vieja'];
+					if ($cantidadBook == 1) {
+						$book_status = 2; //copia unica
+						$cantidadBook = 1;
+					}
 
-							elseif($cantidadBook<$cantidadVieja){
-								$Ids=$this->booksModel->getIdsBooks($isbnViejo);
-								$cantidadVieja=$cantidadVieja-1;
-								for ($i=$cantidadVieja; $i>=$cantidadBook;$i--) {	
-									
-									$param = [
-										'book_id' 	  => $Ids[$i]->book_id, 
-										'category_id'  => $Ids[$i]->category_id	,																				
-										'editorial_id' => $Ids[$i]->editorial_id,										
-										'languaje_id'  => $Ids[$i]->languaje_id,																					
-										'book_status_id' =>$Ids[$i]->book_status_id,
-										
-									];
-								$this->booksModel->deletBook($param);
-								}
-							}
+					$i = 1;
+					foreach ($Ids as $id) {
+						$param = [
+							'book-id' 	  => $id->book_id,
+							'book-title'	 => trim($_POST['book-title']),
+							'book-isbn'		 => trim($_POST['book-isbn']),
+							'book-img'		 => $_FILES['book-img'],
+							'ext-img-vieja' => trim($_POST['ext-vieja']),
+							'book-pages'	 => trim($_POST['book-pages']),
+							'book-category'  => trim($_POST['category-topic']),
+
+							'book-desc'		 => trim($_POST['book-desc']),
+							'book-editorial' => trim($_POST['editorial-id']),
+							'book-vol'		 => trim($_POST['book-vol']),
+							'book-edition'	 => trim($_POST['book-edition']),
+							'book-year'		 => trim($_POST['book-year']),
+							'book-topo'		 => trim($_POST['book-topo']) . '-' . $i,
+							'book-languaje'  => trim($_POST['book-languaje']),
+
+							'book-authors'	 => $_POST['author-list'],
+							'book-status_id' => $book_status,
+							'book-cata'		 => trim($_POST['book-cata']),
+						];
+
+						$this->booksModel->editBook($param, $i);
+						$i++;
+					}
+
+					if ($cantidadBook > $cantidadVieja) {
+						$param = [
+							'book-title'	 => trim($_POST['book-title']),
+							'book-isbn'		 => trim($_POST['book-isbn']),
+							'book-img'		 => $_FILES['book-img'],
+							'ext-img-vieja' => trim($_POST['ext-vieja']),
+							'book-pages'	 => trim($_POST['book-pages']),
+							'book-category'  => trim($_POST['category-topic']),
+							'book-desc'		 => trim($_POST['book-desc']),
+							'book-editorial' => trim($_POST['editorial-id']),
+							'book-vol'		 => trim($_POST['book-vol']),
+							'book-edition'	 => trim($_POST['book-edition']),
+							'book-year'		 => trim($_POST['book-year']),
+							'book-topo'		 => trim($_POST['book-topo']),
+							'book-languaje'  => trim($_POST['book-languaje']),
+							'book-authors'	 => $_POST['author-list'],
+							'book-status_id' => $book_status,
+							'book-cata'		 => trim($_POST['book-cata']),
+						];
+
+						$this->booksModel->addBook($param, $cantidadVieja + 1, $cantidadBook);
+					} elseif ($cantidadBook < $cantidadVieja) {
+						$Ids = $this->booksModel->getIdsBooks($isbnViejo);
+						$cantidadVieja = $cantidadVieja - 1;
+						for ($i = $cantidadVieja; $i >= $cantidadBook; $i--) {
+
+							$param = [
+								'book_id' 	  => $Ids[$i]->book_id,
+								'category_id'  => $Ids[$i]->category_id,
+								'editorial_id' => $Ids[$i]->editorial_id,
+								'languaje_id'  => $Ids[$i]->languaje_id,
+								'book_status_id' => $Ids[$i]->book_status_id,
+
+							];
+							$this->booksModel->deletBook($param);
+						}
+					}
 
 
-							redirect('books/index');							
-				}	
-
-			}									
-			
-				
-		}
-
-//book habilitacion para prestamo
-	public function getsStatusBook(){
-		if(isset($_POST['book_isbn'])){
-			$isbn=$_POST['book_isbn'];
-			
-			$book_availability=$this->booksModel->countBookAvailability($isbn,'Disponible');
-				$quantity=$book_availability->book_cantidad;	
-			if($quantity>0){
-				$book=$this->booksModel->first_book_availability($isbn);
-				echo "<input type=hidden name=book_id value='$book->book_id' >
-					<p>topolographic code :$book->book_topolographic<p>";	
-
-					echo "<h6>disponibilidad de libros para prestar</h6>".$quantity;
-								}
-				else{
-					echo "no hay libro disponible";
+					redirect('books/index');
 				}
+			}
 		}
 
-		
-	}
-		
+		//book habilitacion para prestamo
+		public function getsStatusBook()
+		{
+			if (isset($_POST['book_isbn'])) {
+				$isbn = $_POST['book_isbn'];
 
+				$book_availability = $this->booksModel->countBookAvailability($isbn, 'Disponible');
+				$quantity = $book_availability->book_cantidad;
+				if ($quantity > 0) {
+					$book = $this->booksModel->first_book_availability($isbn);
+					echo "<input type=hidden name=book_id value='$book->book_id' >
+						<p>topolographic code :$book->book_topolographic<p>";
 
-
-
+					echo "<h6>Libros disponibles: </h6>" . $quantity;
+				} else {
+					echo "No disponible";
+				}
+			}
+		} 
 
 		public function delete($book_id){
-			$book=$this->booksModel->getBook($book_id);
+			$book = $this->booksModel->getBook($book_id);
 			$param = [
-				'book_id' 	  => $book->book_id, 
-				'category_id'  => $book->category_id	,																				
-				'editorial_id' => $book->editorial_id,										
-				'languaje_id'  => $book->languaje_id,																					
-				'book_status_id' =>$book->book_status_id,
-				
-			];
-		$this->booksModel->deletBook($param);
-		}
+				'book_id' 	  => $book->book_id,
+				'category_id'  => $book->category_id,
+				'editorial_id' => $book->editorial_id,
+				'languaje_id'  => $book->languaje_id,
+				'book_status_id' => $book->book_status_id,
 
-		
- 	}
+			];
+			$this->booksModel->deletBook($param);
+		}
+	}
 ?>
--- app/loans/views/create.php
+++ app/loans/views/create.php
@@ -1,6 +1,7 @@
-<?php $DateCurrent =  $param['date-current']; 
-    $DateFinishLoan=$param['date-finish-loan']
-        ?>
+<?php
+    $DateCurrent    = $param['date-current'];
+    $DateFinishLoan = $param['date-finish-loan']
+?>
 <div class="col-10 mt-2">
     <h4 class="mb-3">Registrar Prestamo</h4>
     <form method="post" action="<?php echo URL_ROUTE ?>loans/store" target="_top">
@@ -8,64 +9,62 @@
             <div class="col-6">
                 <div class="form-group">
                     <label for="name-user">Socio</label>
-                    <input type="hidden"  id="selected-user">
-                        <div class="container-select">  
+                    <input type="hidden" id="selected-user">
+                    <div class="container-select">
                         <div class="selected-s" id="select-user">Seleccionar usuario</div>
                         <div class="container-options" id="container-user">
-                            <input type="text" name="user-name" class="form-control" id="search_user_loan" required placeholder="Buscar socio por DNI" data-url="<?php echo URL_ROUTE?>Users/searchUserDni "> 
-                            <ul class="options" id="options-user" ></ul>
+                            <input type="text" name="user-name" class="form-control" id="search_user_loan" required placeholder="Buscar socio por DNI" data-url="<?php echo URL_ROUTE ?>Users/searchUserDni ">
+                            <ul class="options" id="options-user"></ul>
                         </div>
-                        </div>
-                </div>  
+                    </div>
+                </div>
             </div>
             <div class="col-6 mt-3">
-                <div class="media" id="user_media">
-
-                </div>     
-            </div> 
-        </div> 
+                <div class="media" id="user_media"></div>
+            </div>
+        </div>
         <hr>
         <div class="row">
             <div class="col-6">
                 <div class="form-group">
-
                     <label for="title-book">Libro</label>
-                    <input type="hidden"  id="selected-book">
+                    <input type="hidden" id="selected-book">
                     <div class="container-select">
-                     <div class="selected-s" id="select-book">Seleccionar book</div>
-                         <div class="container-options" id="container-book">
-                             <input type="text"  class="form-control search" id="search_book_loan" required placeholder="Buscar libro" data-url="<?php echo URL_ROUTE?>Books/searchForLoan"> 
-                              <ul class="options" id="options-book" ></ul>
+                        <div class="selected-s" id="select-book">Seleccionar book</div>
+                        <div class="container-options" id="container-book">
+                            <input type="text" class="form-control search" id="search_book_loan" required placeholder="Buscar libro" data-url="<?php echo URL_ROUTE ?>Books/searchForLoan">
+                            <ul class="options" id="options-book"></ul>
+                        </div>
                     </div>
-                    </div>
-                                          
-                 </div>  
-            </div>
-            <div class="col-6 mt-2" >
-                <div id ="book_media" data-url="<?php echo URL_ROUTE?>Books/getsStatusBook">
-                
+
                 </div>
-
+            </div>
+            <div class="col-6 mt-2">
+                <div id="book_media" data-url="<?php echo URL_ROUTE ?>Books/getsStatusBook"></div>
                 <span id="availability"></span>
-            </div> 
-            
-        </div> 
-        <div class="form-group row">
-            <label for="example-datetime-local-input" class="col-2 col-form-label">fecha actual</label>
-            <div class="col-10">
-            <input class="form-control" type="date"  name="datetime_loan" disabled value='<?php echo date('Y-m-d', strtotime($DateCurrent));?>' id="datetime-loan-current">
-            </div> 
-            <label for="example-datetime-local-input" class="col-2 col-form-label">fecha devolucion</label>
-            <div class="col-10">
-            <input class="form-control" type="date" name="datetime_finish_loan"  value='<?php echo date('Y-m-d', strtotime($DateFinishLoan) );?>' id="datetime-loan">
-            </div> 
-             
-
+            </div>
         </div>
-        
-        
+        <div class="form-group row">
+            <div class="col-6">
+                <label for="example-datetime-local-input" class="col-form-label">Fecha de préstamo</label>
+                <input class="form-control" type="date" name="datetime_loan" disabled value='<?php echo date('Y-m-d', strtotime($DateCurrent)); ?>' id="datetime-loan-current">
+            </div>
+            <div class="col-6">
+                <label for="example-datetime-local-input" class="col-form-label">Fecha de devolución</label>
+                <input class="form-control" type="date" name="datetime_finish_loan" value='<?php echo date('Y-m-d', strtotime($DateFinishLoan)); ?>' id="datetime-loan">
+            </div>
+        </div>
         <div class="form-group mt-5">
             <button type="submit" class="form-control btn btn-primary" id='submit' name="loan-register">Registar prestamo</button>
-        </div>  
-    </form> 
-</div> 
+        </div>
+    </form>
+    <div class="col-6 mt-2">
+        <div class="media">
+            <img class="mr-3" src="<?php echo URL_ROUTE;?>media/images/book/default-cover-book.png" alt="User profile img" style="width: 50px; height:70px;">
+            <div class="media-body">
+                <h5 class="mt-0">Book title extra large for test</h5>
+                <p>Status status status</p>
+            </div>
+        </div>     
+    </div> 
+</div>
